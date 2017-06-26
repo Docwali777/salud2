59fd84e724bc169bb3fef4c70113fb6a1882396e
docwali777 <drgauvin@mgmcare.org>
2017-06-25 22:24:51 -0400

    v4

--- src/pages/components/carousel.js
+++ src/pages/components/carousel.js
@@ -11,29 +11,6 @@ export default class Slideshow extends Component{
   render(){
     return(
       <div className='Slideshow'>
-        <Carousel className='Carousel'>
-      <Carousel.Item>
-        <img width={500} height={500} alt="500x500"  src={heart}/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width={500} height={500} alt="500x500" src={image2}/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img width={500} height={500} alt="500x500" src={image3}/>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      </Carousel>
 
       </div>
     )
